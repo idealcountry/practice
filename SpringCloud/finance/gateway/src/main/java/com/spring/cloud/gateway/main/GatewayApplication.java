9d2d5a0e96193b2fef82b55a2edfc5a38b22b097
liuhaicheng <hik@hik.com>
2021-09-15 10:48:53 +0800

    更新'SpringCloud项目'

--- SpringCloud/finance/gateway/src/main/java/com/spring/cloud/gateway/main/GatewayApplication.java
+++ SpringCloud/finance/gateway/src/main/java/com/spring/cloud/gateway/main/GatewayApplication.java
@@ -36,21 +36,21 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-//    /**
-//     * 创建路由规则
-//     * @param builder -- 路由构造器
-//     * @return 路由规则
-//     */
-//    @Bean
-//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-//        return builder.routes() //开启路由配置
-//                // 匹配路径
-//                .route(f -> f.path("/user/**") // route方法逻辑是一个断言，后续会论述
-//                        // 转发到具体的URI
-//                        .uri("http://localhost:6001"))
-//                // 创建
-//                .build();
-//    }
+    /**
+     * 创建路由规则
+     * @param builder -- 路由构造器
+     * @return 路由规则
+     */
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes() //开启路由配置
+                // 匹配路径
+                .route(f -> f.path("/user/**") // route方法逻辑是一个断言，后续会论述
+                        // 转发到具体的URI
+                        .uri("http://localhost:6001"))
+                // 创建
+                .build();
+    }
 
 //    /**
 //     * 创建路由规则
